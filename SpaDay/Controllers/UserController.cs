0cdd3aebbe279b9ff06390dacf6252b9f93fb1ec
Meghan <stlmeghan@yahoo.com>
2022-10-12 18:37:48 -0500

    model binding is not working

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser.Name;
+                return View("/index");
+            }
+            else
+            {
+                return View("/add");
+            }
+        }
+    }
+
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Models
+{
+    [BindProperties]
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Id { get; }
+        private static int nextID = 1;
+
+        public User()
+        {
+            Id = nextID;
+            nextID++;
+        }
+
+        public User(string name, string email, string password): this()
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
